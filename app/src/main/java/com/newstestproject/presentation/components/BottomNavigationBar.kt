ec9c3d9e47c51777570b6a88bf0af3b411d7f811
summerwise <zelyoniyoleg@yandex.ru>
2022-11-10 00:43:57 +0500

    design fixes

--- app/src/main/java/com/newstestproject/presentation/components/BottomNavigationBar.kt
+++ app/src/main/java/com/newstestproject/presentation/components/BottomNavigationBar.kt
@@ -1,9 +1,10 @@
 package com.newstestproject.presentation.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -14,6 +15,7 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -27,44 +29,60 @@ fun BottomNavigationBar(
     items: List<BottomNavItem>,
     navController: NavController,
     modifier: Modifier = Modifier,
-    backgroundColor: Color,
+    containerColor: Color,
     onItemClick: (BottomNavItem) -> Unit
 ) {
     val backStackEntry = navController.currentBackStackEntryAsState()
-    NavigationBar(
-        modifier = modifier
-            .height(50.dp)
-            .shadow(4.dp),
-        containerColor = backgroundColor,
-    ) {
-        items.forEach { item ->
-            val selected = item.screen.route == backStackEntry.value?.destination?.route
-                    || item.screen.subRoutes?.contains(backStackEntry.value?.destination?.route) ?: false
-            NavigationBarItem(
-                selected = selected,
-                onClick = {
-                    onItemClick(item)
-                },
-                enabled = true,
-                colors =  NavigationBarItemDefaults.colors(
-                    indicatorColor = MaterialTheme.colors.background,
+    Column {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(containerColor)
+                .shadow(4.dp)
+        )
+        NavigationBar(
+            modifier = modifier
+                .height(48.dp)
+                .shadow(
+                    elevation = 9.dp,
+                    spotColor = Color.Black,
+                    ambientColor = Color.Black,
+                    shape = RoundedCornerShape(0.dp),
+                    clip = true
                 ),
-                icon = {
-                    Column(horizontalAlignment = CenterHorizontally) {
-                        Icon(
-                            painter = painterResource(id = item.iconId),
-                            tint = if(selected) MaterialTheme.colors.primaryVariant else Color.Gray,
-                            contentDescription = item.screen.screenName.asString()
-                        )
-                        Text(
-                            text = item.screen.screenName.asString(),
-                            textAlign = TextAlign.Center,
-                            fontSize = 10.sp,
-                            color = if(selected) MaterialTheme.colors.primaryVariant else Color.Gray,
-                        )
+            containerColor = containerColor,
+            tonalElevation = 0.dp,
+        ) {
+            items.forEach { item ->
+                val selected = item.screen.route == backStackEntry.value?.destination?.route
+                        || item.screen.subRoutes?.contains(backStackEntry.value?.destination?.route) ?: false
+                NavigationBarItem(
+                    selected = selected,
+                    onClick = {
+                        onItemClick(item)
+                    },
+                    enabled = true,
+                    colors =  NavigationBarItemDefaults.colors(
+                        indicatorColor = containerColor,
+                    ),
+                    icon = {
+                        Column(horizontalAlignment = CenterHorizontally) {
+                            Icon(
+                                painter = painterResource(id = item.iconId),
+                                tint = if(selected) MaterialTheme.colors.primaryVariant else Color.Gray,
+                                contentDescription = item.screen.screenName.asString()
+                            )
+                            Text(
+                                text = item.screen.screenName.asString(),
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp,
+                                color = if(selected) MaterialTheme.colors.primaryVariant else Color.Gray,
+                            )
+                        }
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
